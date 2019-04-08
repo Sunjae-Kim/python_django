424f4b09ea53d02e53655094659a1cc4ce79f981
sunjae-kim <kimsj9484@gmail.com>
2019-04-08 13:49:55 +0900

    Create board model
     - Make board model migration
     - Create skeleton code for CRUD operation for board model

--- board/urls.py
+++ board/urls.py
@@ -7,4 +7,23 @@ app_name = 'board'
 urlpatterns = [
     path('index/', views.index),  # DOMAIN/board/index
     path('greeting/<str:name>/<str:role>', views.greeting),  # DOMAIN/board/greeting/jason
+
+    # Create
+    # /articles/new => html (작성하는 화면)
+    # /articles/create => DB new record
+    path('articles/new', views.new),
+    path('articles/create', views.create),
+
+    # Read
+    # /articles => html (all articles)
+    # /articles/1 => html (article with 1)
+    path('articles', views.all_list),
+    # path('articles')
+
+    # Update
+    # /articles/1/edit => html (1 article 을 수정하는 화면)
+    # /articles/1/update => DB update article with 1
+
+    # Delete
+    # /articles/1/delete => DB delete article with 1
 ]
--- board/views.py
+++ board/views.py
@@ -2,6 +2,34 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def new(request):
+    pass
+
+
+def create(request):
+    pass
+
+
+def all_list(request):
+    pass
+
+
+def detail(request, id):
+    pass
+
+
+def edit(request, id):
+    pass
+
+
+def update(request, id):
+    pass
+
+
+def delete(request, id):
+    pass
+
+
 def index(request):
     return render(request, 'board/index.html')
 
